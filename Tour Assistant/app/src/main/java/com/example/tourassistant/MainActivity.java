873813ffa5b25b9eddbf5f320fab9a9dd481cfab
Vophong1612 <55780891+Vophong1612@users.noreply.github.com>
2019-11-07 23:17:56 +0700

    custom lại listview, thêm giao diện cho nút add

--- Tour Assistant/app/src/main/java/com/example/tourassistant/MainActivity.java
+++ Tour Assistant/app/src/main/java/com/example/tourassistant/MainActivity.java
@@ -6,10 +6,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
+
+import com.example.tourassistant.Object.Tour;
+import com.example.tourassistant.adapter.TourAdapters;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    Tour tour;
+    ArrayList<Tour> toursList = new ArrayList<Tour>();
+    TourAdapters tourAdapters;
+    ListView lvTours;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +29,19 @@ public class MainActivity extends AppCompatActivity {
         //title vao giua
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.abs_layout);
+
+        addControls();
+        Show();
+    }
+
+    private void Show() {
+        tour = new Tour("HCM - Nha Trang",  10, 15, 0, 1, 2, 3, false, 10, 5,10000, 50000, "https://cdnimg.vietnamplus.vn/uploaded/fsmsr/2018_12_03/31.jpg");
+        toursList.add(tour);
+        tourAdapters = new TourAdapters(MainActivity.this, R.layout.listview_tour_layout, toursList);
+        lvTours.setAdapter(tourAdapters);
+    }
+
+    private void addControls() {
+        lvTours = (ListView) findViewById(R.id.listTour);
     }
 }
