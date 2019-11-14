d73601b69a7703e749286796d5ce64ad12be0209
baophuc53 <55937237+baophuc53@users.noreply.github.com>
2019-11-14 13:55:57 +0700

    Tạo Mapactivity

---
+++ Tour Assistant/app/src/main/java/com/example/tourassistant/Activity/MapsActivity.java
@@ -0,0 +1,107 @@
+package com.example.tourassistant.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private boolean mLocationPermissionsGranted = false;
+    private GoogleMap mMap;
+    SupportMapFragment mapFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        getLocationPermission();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+    }
+
+    private void initMap() {
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    private void getLocationPermission(){
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionsGranted = true;
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionsGranted = false;
+        switch (requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if(grantResults.length > 0){
+                    for (int i = 0; i<grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionsGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionsGranted = true;
+                    //inittialize our map
+                    initMap();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Toast.makeText(MapsActivity.this, "Map is ready", Toast.LENGTH_SHORT).show();
+        Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.US);
+        try {
+            List<Address> FoundAddresses = geocoder.getFromLocationName(
+                    " Main Ave. Ohio ", 5);
+            double lat = FoundAddresses.get(0).getLatitude();
+            double lng = FoundAddresses.get(0).getLongitude();
+            LatLng ohio = new LatLng(lat, lng);
+            mMap.addMarker(new MarkerOptions().position(ohio).title("ohio"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ohio, 15.0f));
+        } catch (Exception e) {
+            Log.e("Goecoder>>>", "ERROR " + e.getMessage());
+        }
+    }
+}
