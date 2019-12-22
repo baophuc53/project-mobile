81ef3f9cd09b1ccc27a6f28007039e313dd36d0d
huuphuoc1xx <34543227+huuphuoc1xx@users.noreply.github.com>
2019-12-22 22:41:31 +0700

    Update MyFirebaseService.java

--- Tour Assistant/app/src/main/java/com/ygaps/travelapp/Activity/Service/MyFirebaseService.java
+++ Tour Assistant/app/src/main/java/com/ygaps/travelapp/Activity/Service/MyFirebaseService.java
@@ -10,33 +10,75 @@ import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.Settings;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.ygaps.travelapp.Activity.ListTourActivity;
 import com.ygaps.travelapp.Activity.MainActivity;
 import com.ygaps.travelapp.Activity.R;
+import com.ygaps.travelapp.Api.MyAPIClient;
+import com.ygaps.travelapp.Api.UserService;
+import com.ygaps.travelapp.model.DefaultResponse;
+import com.ygaps.travelapp.model.TokenRequest;
+
+import java.util.Map;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class MyFirebaseService extends FirebaseMessagingService {
     private static final String TAG = "MyFirebaseService";
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+        RemoteMessage.Notification notification=remoteMessage.getNotification();
         // handle a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        if (notification != null) {
+            Log.d(TAG, "Message Notification Body: " + notification.getBody());
 
-            sendNotification(remoteMessage.getNotification().getBody());
+            sendNotification(notification.getBody());
         }
     }
 
     @Override
     public void onNewToken(String token) {
         Log.d(TAG, "Refreshed token: " + token);
+
+        TokenRequest tokenRequest = new TokenRequest();
+        tokenRequest.setFcmToken(token);
+        tokenRequest.setPlatform(1);
+        tokenRequest.setAppVersion("1.0");
+
+        String deviceId;
+        deviceId = getDeviceId(MyFirebaseService.this);
+        tokenRequest.setDeviceId(deviceId);
+
+        UserService userService= MyAPIClient.getInstance().getAdapter().create(UserService.class);
+        userService.RegisterToken(tokenRequest, new Callback<DefaultResponse>() {
+            @Override
+            public void success(DefaultResponse defaultResponse, Response response) {
+                Log.i("register firebase", "success");
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.i("register firebase", "failure");
+            }
+        });
     }
 
+    private String getDeviceId(Context context) {
+        String androidId = Settings.Secure.getString(
+                context.getContentResolver(), Settings.Secure.ANDROID_ID);
+
+        return androidId;
+    }
 
 
     private void sendNotification(String messageBody) {
@@ -48,7 +90,7 @@ public class MyFirebaseService extends FirebaseMessagingService {
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
         NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this, channelId)
+                new NotificationCompat.Builder(this,channelId)
                         .setSmallIcon(R.drawable.ic_launcher_background)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_background))
                         .setContentTitle(getString(R.string.project_id))
